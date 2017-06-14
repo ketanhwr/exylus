82862386793a67b181de5bfeb747bb95f73efb58
Ketan Gupta <ketan19972010@gmail.com>
2017-06-14 13:20:40 +0530

    Fancy greeting

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -10,5 +10,13 @@
 void kernel_main ()
 {
 	terminal_initialize();
-	terminal_writestring("Exylus..!\n");
+	terminal_writestring("  ______            _           \n");
+	terminal_writestring(" |  ____|          | |          \n");
+	terminal_writestring(" | |__  __  ___   _| |_   _ ___ \n");
+	terminal_writestring(" |  __| \\ \\/ / | | | | | | / __|\n");
+	terminal_writestring(" | |____ >  <| |_| | | |_| \\__ \\\n");
+	terminal_writestring(" |______/_/\\_\\\\__, |_|\\__,_|___/\n");
+	terminal_writestring("               __/ |            \n");
+	terminal_writestring("              |___/             \n");
 }
+                                                  
