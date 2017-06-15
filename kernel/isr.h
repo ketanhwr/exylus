d178b5b83a7add1d0d4d27dec8624e7d3b4134dd
Ketan Gupta <ketan19972010@gmail.com>
2017-06-16 00:07:25 +0530

    changes unsigned int to uint32_t

--- kernel/isr.h
+++ kernel/isr.h
@@ -9,10 +9,10 @@
 
 typedef struct registers
 {
-	unsigned int gs, fs, es, ds;
-	unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
-	unsigned int int_no, err_code;
-	unsigned int eip, cs, eflags, useresp, ss;
+	uint32_t gs, fs, es, ds;
+	uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+	uint32_t int_no, err_code;
+	uint32_t eip, cs, eflags, useresp, ss;
 } registers_t;
 
 void isr_handler(registers_t *regs);
