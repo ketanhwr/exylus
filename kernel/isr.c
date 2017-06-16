9be598f56dafe6f65c3f470f9a6b45d08241ca7e
Ketan Gupta <ketan19972010@gmail.com>
2017-06-16 10:14:33 +0530

    Adds exception message

--- kernel/isr.c
+++ kernel/isr.c
@@ -9,11 +9,45 @@
 
 #include "tty.h"
 
+char *exception_messsage[] = {
+	"Division By Zero Exception",
+	"Debug Exception",
+	"Non Maskable Interrupt Exception",
+	"Breakpoint Exception",
+	"Into Detected Overflow Exception",
+	"Out of Bounds Exception",
+	"Invalid Opcode Exception",
+	"No Coprocessor Exception",
+	"Double Fault Exception",
+	"Coprocessor Segment Overrun Exception",
+	"Bad TSS Exception",
+	"Segment Not Present Exception",
+	"Stack Fault Exception",
+	"General Protection Fault Exception",
+	"Page Fault Exception",
+	"Unknown Interrupt Exception",
+	"Coprocessor Fault Exception",
+	"Alignment Check Exception",
+	"Machine Check Exception",
+	"Reserved Exception",
+	"Reserved Exception",
+	"Reserved Exception",
+	"Reserved Exception",
+	"Reserved Exception",
+	"Reserved Exception",
+	"Reserved Exception",
+	"Reserved Exception",
+	"Reserved Exception",
+	"Reserved Exception",
+	"Reserved Exception",
+	"Reserved Exception",
+	"Reserved Exception"
+};
+
 void isr_handler(registers_t *regs)
 {
 	if (regs->int_no < 32) {
-		terminal_writestring("Interrupt: ");
-		terminal_writeint(regs->int_no);
+		terminal_writestring(exception_messsage[regs->int_no]);
 		terminal_writestring("\n");
 	}
 }
