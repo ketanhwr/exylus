fa81d36324f2d7a41d139fc1ab9e10f5094f9f02
Ketan Gupta <ketan19972010@gmail.com>
2017-06-16 19:35:30 +0530

    Bug fix in tty scrolling

--- kernel/tty.c
+++ kernel/tty.c
@@ -78,17 +78,21 @@ void terminal_putchar(char c)
 	if (c == '\n') {
 		++terminal_row;
 		terminal_column = 0;
-		
+
+		if (terminal_row == VGA_HEIGHT - 1) {
+			terminal_scroll();
+		}
+
 		return;
 	} else {
 		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	}
 
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
+		if (++terminal_column == VGA_WIDTH) {
+			terminal_column = 0;
 
-		if (++terminal_row == VGA_HEIGHT) {
-			terminal_scroll();
+			if (++terminal_row == VGA_HEIGHT - 1) {
+				terminal_scroll();
+			}
 		}
 	}
 }
