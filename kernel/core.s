d19cae646a4a4292f24c64f6becd36d9af48efbb
Ketan Gupta <ketan19972010@gmail.com>
2017-06-18 16:45:08 +0530

    Adds macro for IRQ

--- kernel/core.s
+++ kernel/core.s
@@ -62,19 +62,19 @@ idt_flush:
 	.global isr\count
 	.type isr\count, @function
 	isr\count:
-    	cli
-    	pushl $0
-    	pushl $\count
-    	jmp isr_common_stub
+		cli
+		pushl $0
+		pushl $\count
+		jmp isr_common_stub
 .endm
 
 .macro ISR_ERRCODE count
 	.global isr\count
 	.type isr\count, @function
 	isr\count:
-    	cli
-    	pushl $\count
-    	jmp isr_common_stub
+		cli
+		pushl $\count
+		jmp isr_common_stub
 .endm
 
 ISR_NOERRCODE 0
@@ -135,134 +135,32 @@ isr_common_stub:
 	addl $8,%esp
 	iret
 
-.global irq0
-.global irq1
-.global irq2
-.global irq3
-.global irq4
-.global irq5
-.global irq6
-.global irq7
-.global irq8
-.global irq9
-.global irq10
-.global irq11
-.global irq12
-.global irq13
-.global irq14
-.global irq15
+.macro IRQ val1, val2
+	.global irq\val1
+	.type irq\val1, @function
+	irq\val1:
+		cli
+		pushl $0
+		pushl $\val2
+		jmp irq_common_stub
+.endm
 
-.type irq0, @function
-irq0:
-	cli
-	pushl $0
-	pushl $32
-	jmp irq_common_stub
-
-.type irq1, @function
-irq1:
-	cli
-	pushl $0
-	pushl $33
-	jmp irq_common_stub
-
-.type irq2, @function
-irq2:
-	cli
-	pushl $0
-	pushl $34
-	jmp irq_common_stub
-
-.type irq3, @function
-irq3:
-	cli
-	pushl $0
-	pushl $35
-	jmp irq_common_stub
-
-.type irq4, @function
-irq4:
-	cli
-	pushl $0
-	pushl $36
-	jmp irq_common_stub
-
-.type irq5, @function
-irq5:
-	cli
-	pushl $0
-	pushl $37
-	jmp irq_common_stub
-
-.type irq6, @function
-irq6:
-	cli
-	pushl $0
-	pushl $38
-	jmp irq_common_stub
-
-.type irq7, @function
-irq7:
-	cli
-	pushl $0
-	pushl $39
-	jmp irq_common_stub
-
-.type irq8, @function
-irq8:
-	cli
-	pushl $0
-	pushl $40
-	jmp irq_common_stub
-
-.type irq9, @function
-irq9:
-	cli
-	pushl $0
-	pushl $41
-	jmp irq_common_stub
-
-.type irq10, @function
-irq10:
-	cli
-	pushl $0
-	pushl $42
-	jmp irq_common_stub
-
-.type irq11, @function
-irq11:
-	cli
-	pushl $0
-	pushl $43
-	jmp irq_common_stub
-
-.type irq12, @function
-irq12:
-	cli
-	pushl $0
-	pushl $44
-	jmp irq_common_stub
-
-.type irq13, @function
-irq13:
-	cli
-	pushl $0
-	pushl $45
-	jmp irq_common_stub
-
-.type irq14, @function
-irq14:
-	cli
-	pushl $0
-	pushl $46
-	jmp irq_common_stub
-
-.type irq15, @function
-irq15:
-	cli
-	pushl $0
-	pushl $47
-	jmp irq_common_stub
+IRQ 0, 32
+IRQ 1, 33
+IRQ 2, 34
+IRQ 3, 35
+IRQ 4, 36
+IRQ 5, 37
+IRQ 6, 38
+IRQ 7, 39
+IRQ 8, 40
+IRQ 9, 41
+IRQ 10, 42
+IRQ 11, 43
+IRQ 12, 44
+IRQ 13, 45
+IRQ 14, 46
+IRQ 15, 47
 
 .type irq_common_stub, @function
 irq_common_stub:
